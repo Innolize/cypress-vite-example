836f1367b66d5c39698ee14c61fec5c663c6e490
Roman Mettler <roman.mettler@gmail.com>
2023-04-09 04:45:20 -0300

    Implement auth provider

---
+++ src/providers/authProvider.tsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from "react";
+import { IUser } from "../interfaces/User";
+
+export const AuthContext = createContext<IAuthorProviderValue>({
+  setAuth: () => console.log("setAuth not implemented"),
+  user: null,
+});
+
+interface IAuthorProviderValue {
+  user: IUser | null;
+  setAuth: (user: IUser) => void;
+}
+
+export const AuthProvider = (props: any) => {
+  const [user, setUser] = useState<IUser | null>(null);
+
+  const setAuth = (user: IUser) => {
+    setUser(user);
+  };
+
+  const value: IAuthorProviderValue = {
+    user,
+    setAuth,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>{props.children}</AuthContext.Provider>
+  );
+};
+
