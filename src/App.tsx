78ea524d1e69838147213e34c6782997526c4d5c
Roman Mettler <roman.mettler@gmail.com>
2023-04-09 01:23:32 -0300

    Create mainpage component and setup basic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+
+const MainPage = () => {
+  return (
+    <div>
+      <a href="https://reactjs.org" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+      <h1>React</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" Component={MainPage}></Route>
+        </Routes>
       </div>
-      <h1>React</h1>
-    </div>
+    </BrowserRouter>
   );
 }
 
