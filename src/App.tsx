c432f3b8e195b104bc292c53c795f581b90c6e89
Roman Mettler <roman.mettler@gmail.com>
2023-04-09 00:41:58 -0300

    Initialize project
    * Utilize vite to create a typescript project
    * Clear main page

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React</h1>
+    </div>
+  );
+}
+
+export default App;
+
