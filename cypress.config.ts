3fa49aaa0f49fdf72293f54c446bf844d255a9cd
Roman Mettler <roman.mettler@gmail.com>
2023-04-10 01:54:19 -0300

    Set up cypress and create basic tests

---
+++ cypress.config.ts
@@ -0,0 +1,17 @@
+import path from 'path'
+import { defineConfig } from 'cypress'
+import vitePreprocessor from 'cypress-vite'
+
+const __dirname = path.resolve()
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on) {
+      on(
+        'file:preprocessor',
+        vitePreprocessor(path.resolve(__dirname, './vite.config.ts'))
+      )
+    },
+    supportFile: false,
+  },
+})
