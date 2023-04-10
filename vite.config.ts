d1944f2a695653e22d8515f26caad4c81eea71a7
Roman Mettler <roman.mettler@gmail.com>
2023-04-10 08:40:04 -0300

    Allow to set a custom PORT in the vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import eslint from "vite-plugin-eslint";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import eslint from 'vite-plugin-eslint'
+import { config } from 'dotenv'
+
+config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: Number(process.env.PORT as string),
+  },
   plugins: [react(), eslint()],
-});
-
+})
