b053251df96e565d5ec3f68fe9f0ddba0534e930
Roman Mettler <roman.mettler@gmail.com>
2023-04-10 23:09:38 -0300

    Allow cypress to collect code coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,6 @@
 import path from 'path'
 import { defineConfig } from 'cypress'
+import task from '@cypress/code-coverage/task'
 import vitePreprocessor from 'cypress-vite'
 
 const __dirname = path.resolve()
@@ -8,12 +9,14 @@ export default defineConfig({
   video: false,
   screenshotOnRunFailure: false,
   e2e: {
-    setupNodeEvents(on) {
+    setupNodeEvents(on, config) {
       on(
         'file:preprocessor',
         vitePreprocessor(path.resolve(__dirname, './vite.config.ts'))
       )
+      task(on, config)
+
+      return config
     },
-    supportFile: false,
   },
 })
