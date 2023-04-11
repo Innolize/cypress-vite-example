11534ef89efd824c73cdb49dfb4746f6a4294926
Roman Mettler <roman.mettler@gmail.com>
2023-04-11 00:10:17 -0300

    should throw if code coverage is not met

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,46 +1,50 @@
 /// <reference types="cypress"/>
 
-// describe('login', () => {
-//   beforeEach(() => {
-//     cy.visit('/signin')
-//   })
-//   it('Should display errors if email and password parameters are incorrect', () => {
-//     cy.get('#signin-form').as('form')
+describe('login', () => {
+  // beforeEach(() => {
+  //   cy.visit('/signin')
+  // })
 
-//     cy.get('@form').find('input[name=email]').type('12345')
+  it(()=>{
+    cy.visit('/signin')
+  })
+  // it('Should display errors if email and password parameters are incorrect', () => {
+  //   cy.get('#signin-form').as('form')
 
-//     cy.get('@form').find('input[name=password]').type('12345')
+  //   cy.get('@form').find('input[name=email]').type('12345')
 
-//     cy.get('@form').contains('Sign In').click()
+  //   cy.get('@form').find('input[name=password]').type('12345')
 
-//     cy.get('@form').contains('Enter a valid email')
-//     cy.get('@form').contains(
-//       'Password should be of minimum 8 characters length'
-//     )
-//   })
+  //   cy.get('@form').contains('Sign In').click()
 
-//   it('Should display empty field errors ', () => {
-//     cy.get('#signin-form').as('form')
+  //   cy.get('@form').contains('Enter a valid email')
+  //   cy.get('@form').contains(
+  //     'Password should be of minimum 8 characters length'
+  //   )
+  // })
 
-//     cy.get('@form').find('input[name=email]').click()
+  // it('Should display empty field errors ', () => {
+  //   cy.get('#signin-form').as('form')
 
-//     cy.get('@form').find('input[name=password]').click()
+  //   cy.get('@form').find('input[name=email]').click()
 
-//     cy.get('@form').contains('Sign In').click()
+  //   cy.get('@form').find('input[name=password]').click()
 
-//     cy.get('@form').contains('Email is required')
-//     cy.get('@form').contains('Password is required')
-//   })
+  //   cy.get('@form').contains('Sign In').click()
 
-//   it('Should be redirected to "/princing" if credentials are correct', () => {
-//     cy.get('#signin-form').as('form')
+  //   cy.get('@form').contains('Email is required')
+  //   cy.get('@form').contains('Password is required')
+  // })
 
-//     cy.get('@form').find('input[name=email]').type('test@gmail.com')
+  // it('Should be redirected to "/princing" if credentials are correct', () => {
+  //   cy.get('#signin-form').as('form')
 
-//     cy.get('@form').find('input[name=password]').type('password')
+  //   cy.get('@form').find('input[name=email]').type('test@gmail.com')
 
-//     cy.get('@form').contains('Sign In').click()
+  //   cy.get('@form').find('input[name=password]').type('password')
 
-//     cy.url().should('eq', `${Cypress.config().baseUrl}/pricing`)
-//   })
-// })
+  //   cy.get('@form').contains('Sign In').click()
+
+  //   cy.url().should('eq', `${Cypress.config().baseUrl}/pricing`)
+  // })
+})
